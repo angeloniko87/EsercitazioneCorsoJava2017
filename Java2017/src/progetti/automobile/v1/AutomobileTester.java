bff052bc5a93d8f3afa6ddb6e42a5ea83aac084f
Angelo <Angelo@DESKTOP-Q4NLJIT>
2016-11-27 09:53:35 +0100

    java 2017

--- Java2017/src/progetti/automobile/v1/AutomobileTester.java
+++ Java2017/src/progetti/automobile/v1/AutomobileTester.java
@@ -3,7 +3,7 @@ package progetti.automobile.v1;
 public class AutomobileTester {
 
 	public static void main(String[] args) {
-		System.out.println("automobile");
+		System.out.println("automoile");
 	}
 
 }
